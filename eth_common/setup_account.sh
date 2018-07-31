3b339becc03a32551c810a095832c7516b5c6bb5
dozcan <dogasava@gmail.com>
2018-07-31 13:29:16 +0300

    genesis update

--- eth_common/setup_account.sh
+++ eth_common/setup_account.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ad = `geth --password password account new`
-geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*"  --mine --minerthreads=1 --etherbase 0 --unlock 0 --password password --networkid 11 console
+geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*"  --mine --minerthreads=1 
+--etherbase 0 --unlock 0 --password password  console
