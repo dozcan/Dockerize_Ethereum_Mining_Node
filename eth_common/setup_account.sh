174c2aa132bc081b814199aa8165e79b92fe13b2
dozcan <dogasava@gmail.com>
2018-07-31 16:22:27 +0300

    rpcport

--- eth_common/setup_account.sh
+++ eth_common/setup_account.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ad = `geth --password password account new`
-geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*"  --mine --minerthreads=1 
+geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcport 8545 --mine --minerthreads=1 
 --etherbase 0 --unlock 0 --password password  console
