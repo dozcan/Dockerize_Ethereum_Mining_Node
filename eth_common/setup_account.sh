9aaed6950b71a10f63df2a25d79049a5589feb70
doga ozcan <dogasava@gmail.com>
2018-11-03 16:50:56 +0300

    Update setup_account.sh

--- eth_common/setup_account.sh
+++ eth_common/setup_account.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ad = `geth --password password account new`
-geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcport 8545 --mine --minerthreads=1 --etherbase 0 --unlock 0 --password password 
+geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcport 8545 --mine --minerthreads=1 --etherbase 0 --unlock 0 --password password console
