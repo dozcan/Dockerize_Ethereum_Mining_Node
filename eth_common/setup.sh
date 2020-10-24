67df331520bfd4e51913cc1b099ec231407b213f
doga ozcan <dogasava@gmail.com>
2020-10-24 16:36:01 +0300

    Update setup.sh

--- eth_common/setup.sh
+++ eth_common/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 geth --password password account new
-geth --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcport 8545 --mine --minerthreads=1 --etherbase 0 --unlock 0 --password password
+geth --rpc --rpcaddr "0.0.0.0" --jsonrpc-cors all --rpccorsdomain "*" --rpcport 8545 --mine --minerthreads=1 --etherbase 0 --unlock 0 --password password
